6d021336dfb5fd7cf27ceb3d93f01eb976527b90
flora0420 <flora.xushujing@gmail.com>
2022-03-28 15:25:28 -0700

    get rid of more trailing whitespace

--- first_seed_proj/app.py
+++ first_seed_proj/app.py
@@ -1,6 +1,16 @@
 """first_seed_proj.app: Skeleton of a function."""
 
 
-def main(arg_one: str, arg_two: int):
-    """Main function."""
-    return arg_one * arg_two
+def main(str_torepeat: str, n_rep: int) -> str:
+    """return a string that repeat the given str n times
+
+    Args:
+        str_torepeat (str): [description]
+        n_rep (int): [description]
+
+    Returns:
+        str: [description]
+    >>> from first_seed_proj.app import main
+    >>> assert main('a', 3) == 'aaa'
+    """
+    return str_torepeat * n_rep
